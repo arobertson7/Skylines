cc886adbe8ad6d6a08526089e56215c0ffb6d807
Austin Robertson <robertsonaustin27@gmail.com>
2024-12-04 13:36:37 -0500

    decoupled quite a bit, began implementing button functionality

--- javascript.js
+++ javascript.js
@@ -1,18 +1,21 @@
-
-function City(name, image, hint) {
-    this.name = name;
-    this.image = image;
-    this.hint = hint;
+// City class
+class City {
+    constructor(name, image, hint)
+    {
+        this.name = name;
+        this.image = image;
+        this.hint = hint;
+    }
 }
 
-// U.S.cities
+// U.S. cities
 const ny = new City("New York City", "./images/nyc.jpg", "The greatest city in the world (Unopinionated).");
 const la = new City("Los Angeles", "./images/la.avif", '\u266B' + " Californiaaa.. knows how to party.." + '\u266B' + ". In the cityyyy of __. " + '\u266B');
 const sf = new City("San Francisco", "./images/sf.jpg", "They named their most famous bridge with a different color than it actually is.");
 const seattle = new City("Seattle", "./images/seattle.jpg", "That's the space needle.");
 const chicago = new City("Chicago", "./images/chicago.avif", "chicago hint");
 const lv = new City("Las Vegas", "./images/lv.avif", "lv hint");
-const philly = new City("Philadelphia", "images/phi.jpg", "philly hint");
+const philly = new City("Philadelphia", "./images/philly.jpg", "philly hint");
 const dc = new City("Washington D.C.", "./images/dc.jpg", "dc hint");
 const miami = new City("Miami", "./images/miami.webp", "miami hint");
 const atl = new City("Atlanta", "./images/atl.webp", "atl hint");
@@ -99,45 +102,6 @@ function indexAlreadyUsed(usedIndexArray, newIndex) {
     return false; // not yet used
 }
 
-// function for handling each new round
-/* function nextRound() {
-    // Set picture
-    let cityPic = cityQueue.peek().image;
-    let picContainer = document.getElementById("city-pic");
-    picContainer.src=cityPic;
-
-    // Set random answer choices
-    // Start with ensuring that the correct city is in one of them, a random one;
-    let answerButtonIndex = randomIndex(4);
-    choices.childNodes[answerButtonIndex].textContent = cityQueue.peek().name;
-
-    let usedIndexes = []; // for storing city indexes which were already used
-    
-    // push index of the answer city to the usedIndexes array so it is not used below for another answer choice button
-    for (let i = 0; i < cities.length; i++)
-    {
-        if (cities[i].name == cityQueue.peek().name())
-        {
-            usedIndexes.push(i);
-        }
-    }
-
-    //Set the other 3 buttons with random city names
-    for (let i = 0; i < 4; i++)
-    {
-        let randomCityIndex;
-        if (i != answerButtonIndex) // go through all 4 buttons but skip the one holding the correct answer
-        {
-            do {randomCityIndex = randomIndex(cities.length);
-            }
-            while (indexAlreadyUsed(usedIndexes, randomCityIndex));
-            usedIndexes.push(randomCityIndex);
-            choices.childNodes[i].textContent = cities[randomCityIndex].name;
-        }
-
-    }
-}
-    */
 
 
 /* End helper functions
@@ -147,8 +111,9 @@ function indexAlreadyUsed(usedIndexArray, newIndex) {
 */
 
 
-// Start new game
-function PlayGame() {
+
+function setupGameDisplay()
+{
     // Setup game panel for new game
     const gamePanel = document.querySelector(".game-panel");
     gamePanel.removeChild(startButton);
@@ -171,11 +136,13 @@ function PlayGame() {
 
     //Questions section
     const questions = document.createElement("div");
+    questions.id = "questions";
     questions.classList.add("questions");
     gamePanel.appendChild(questions);
     questions.appendChild(document.createElement("h2"));
     questions.childNodes[0].textContent = "Which city is this?";
     let choices = document.createElement("div");
+    choices.id = "choices";
     choices.classList.add("choices");
     questions.appendChild(choices);
     // add 4 answer choice buttons (initialized here with no text content)
@@ -186,6 +153,7 @@ function PlayGame() {
 
     // Hints section
     let hints = document.createElement("div");
+    hints.id = "hints";
     hints.classList.add("hints");
     gamePanel.appendChild(hints);
     let hintButton = document.createElement("button");
@@ -194,8 +162,9 @@ function PlayGame() {
     let hintsRemaining = document.createElement("p");
     hintsRemaining.textContent = "3 remaining";
     hints.appendChild(hintsRemaining);
+}
 
-
+function setupCitiesQueue() {
     // Setup the queue of randomized cities for new game
     let rounds = 10;
     let numOfCities = cities.length; // number of cities in the cities array
@@ -209,17 +178,10 @@ function PlayGame() {
         usedIndexes.push(randomCityIndex);
         cityQueue.push(cities[randomCityIndex]);
     }
+}
 
-    // Begin the game
-    /* while (!cityQueue.isEmpty())
-    {
-        nextRound(); // This might cause an issue, it might run through all rounds in one go.
-    }
-        */
-
-    // The below needs to take place basically each round
-
-    // Set picture
+function setupNewRound() {
+    // Set picture to the correct answer city
     let cityPic = cityQueue.peek().image;
     let picContainer = document.getElementById("city-pic");
     picContainer.src=cityPic;
@@ -256,6 +218,51 @@ function PlayGame() {
     }
 }
 
+function checkAnswer {
+    
+}
+
+function playRound() {
+    // Enable functionality for answer choice buttons
+    const buttons = choices.childNodes;
+    buttons.forEach((button) => {
+        // change color on mouse enter
+        button.addEventListener("mouseenter", () => {
+            button.style.backgroundColor = "white";
+            button.style.color = "black";
+            button.style.boxShadow = "7px 4px 5px gray";
+        })
+        // revert on mouse out
+        button.addEventListener("mouseout", () => {
+            button.style.backgroundColor = null; // null reverts the style back to how it is set in the stylesheet (removes alterations made here in js)
+            button.style.color = null;
+            button.style.boxShadow = null;
+        })
+
+        button.addEventListener("click", () => {
+
+        })
+    })
+}
+
+
+// Play new game
+function PlayGame() {
+    
+    // Initialize the right side of the display for a new game
+    setupGameDisplay();
+
+    // Set up a new queue of n random cities for the game
+    setupCitiesQueue();
+
+    // Populate image and random answer choices for the buttons
+    setupNewRound();
+
+    //while (!cityQueue.isEmpty())
+    playRound();
+}
+
+// Start new game button
 let startButton = document.querySelector(".start-button");
 startButton.addEventListener("click", () => {
     PlayGame();
